7030951e7da73795d105540b9a2b6205ffabac52
TALLESTbrandon <brandon_zapata11@icloud.com>
2025-07-21 14:00:48 -0400

    my flow chart uppload

--- python_1/test.py
+++ python_1/test.py
@@ -1,39 +1,35 @@
 import random
 
-# this is my list
-
-countries =[
+countries = [
   {"name":"France","continent": "Europe"},
   {"name":"United States of America","continent": "North America"},
-  {"name":"England","continent": "Europe"}, 
+  {"name":"England","continent": "Europe"},
   {"name":"Germany","continent": "Europe"},
   {"name":"Guatemala","continent": "Central America"},
   {"name":"Mexico","continent": "North America"},
   {"name":"Ecuador","continent": "South America"},
-  {"name":"Switerland","continent": "Europe"},
+  {"name":"Switzerland","continent": "Europe"},
   {"name":"Japan","continent": "Asia"},
-  {"name":"Columbia","continent": "South America"},
+  {"name":"Colombia","continent": "South America"},
   {"name":"Brazil","continent": "South America"},
   {"name":"India","continent": "Asia"},
-  {"name":"Canada","continent": "North America",}
-  ]
+  {"name":"Canada","continent": "North America"}
+]
 
-# theses are my variables
-secret =  random.choice(countries)
+secret = random.choice(countries)
+
 print("I'm thinking of a country. Try and guess which one!")
 print(f"Hint: It's in {secret['continent']}")
 print(f"Hint: It starts with the letter '{secret['name'][0]}'")
+
 guess = ""
 tries = 0
 
-# while tru loop
-
-while guess != secret:
+while guess != secret["name"]:
     guess = input("Your guess: ").strip().title()
     tries += 1
-#. this is the logical statements
-if guess == secret:
-    print(f" Nice job you got it! It was {secret}. You got it in {tries} tries.")
-else:
-    print("Not quite, try again tho.")    
-   
+
+    if guess == secret["name"]:
+        print(f" Nice job, you got it! It was {secret['name']}. You got it in {tries} tries.")
+    else:
+        print(" Not quite, try again tho.")
