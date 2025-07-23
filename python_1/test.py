68dc7ad6ff7737fa51f128c74dd21b4952df7287
TALLESTbrandon <brandon_zapata11@icloud.com>
2025-07-23 11:47:22 -0400

    New python code

--- python_1/test.py
+++ python_1/test.py
@@ -1,5 +1,5 @@
 import random
-
+#this is the list of all the countrys.
 countries = [
   {"name":"France","continent": "Europe"},
   {"name":"United States of America","continent": "North America"},
@@ -21,15 +21,18 @@ secret = random.choice(countries)
 print("I'm thinking of a country. Try and guess which one!")
 print(f"Hint: It's in {secret['continent']}")
 print(f"Hint: It starts with the letter '{secret['name'][0]}'")
-
+# this shows how much tries the player takes
 guess = ""
 tries = 0
 
 while guess != secret["name"]:
     guess = input("Your guess: ").strip().title()
     tries += 1
+#Below this message is my custom message. What this does is reuse the lines of code by a singular sentence of code.
+    def secret_guesser():
 
-    if guess == secret["name"]:
-        print(f" Nice job, you got it! It was {secret['name']}. You got it in {tries} tries.")
-    else:
-        print(" Not quite, try again tho.")
+        if guess == secret["name"]:
+            print(f" Nice job, you got it! It was {secret['name']}. You got it in {tries} tries.")
+        else:
+            print(" Not quite, try again tho.")
+    secret_guesser()
