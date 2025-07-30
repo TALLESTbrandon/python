c154d481a9f6c412fee6a082846e72cf7bfb5ec0
TALLESTbrandon <brandon_zapata11@icloud.com>
2025-07-30 14:12:06 -0400

    this is my work for today

--- 2d_game_mechanics_1/soccer-super-star/scripts/player.gd
+++ 2d_game_mechanics_1/soccer-super-star/scripts/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 1000
+@export var JUMP_VELOCITY = -250.0
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var gravity = 800
 
 
 
